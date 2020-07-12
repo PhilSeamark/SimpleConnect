29e500e04f48acbe3919feef60b0517a3fa51065
Philip Seamark <phseamar@microsoft.com>
2020-07-13 08:14:33 +1200

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AnalysisServices.Tabular;
+
+namespace SimpleConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+
+            server.Connect("powerbi://api.powerbi.com/v1.0/myorg/PBICAT AAS Migration Test");
+            Database database = server.Databases[0];
+            Model model = database.Model;
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
